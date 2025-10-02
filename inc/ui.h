ae7858f4e48fbd05c83850137ffd63819ffd6302
jmac-lin-oculo <master.scratchman@gmail.com>
2025-10-02 19:51:04 +0100

    caught up UI with new stui changes

--- inc/ui.h
+++ inc/ui.h
@@ -23,7 +23,10 @@ VerticalBox (children = {
         HorizontalBox (children = {
             SizeLimiter (max_size = [24, -1], child = ListView : "artist_list" (elements = { "first artist", "second" }, show_numbers = 0, enabled = 0)),
             VerticalDivider (),
-            TextArea : "album_detail_area" (text = "artist details go here")
+            VerticalBox (children = {
+                TextArea : "artist_detail_area" (text = "artist details go here"),
+                ListView : "artist_album_list" (elements = { "the 33rd album", "the 34th album" })
+            })
         }),
         HorizontalBox (children = {
             SizeLimiter (max_size = [24, -1], child = ListView : "album_list" (elements = { "first album", "second" }, show_numbers = 0)),
@@ -41,6 +44,13 @@ VerticalBox (children = {
             TextArea : "track_detail_area" (text = "track details go here")
         })
     }, tab_titles = { "artists", "albums", "playlists", "tracks" }, current_tab = 1),
-    Label (text = "shortcut hints")
+    Label (text = "[ctrl+F] search  |  [ctrl+A] import  |  [ctrl+M] modify  |  [ctrl+E] export  |  [ctrl+,] config")
+
+    // TODO: the import shortcut will change to 'create' when the playlists tab is selected, and will not be visible when the artists tab is selected
+    // TODO: the search and import shortcuts will jump to their own pages
+    // TODO: the export shortcut will only be visible if playlists is selected
+    // TODO: there will be a summary of how many albums you own
+    // TODO: each tab child will show different stuff depending whether its albums, artists, playlists, or tracks
+    // TODO: the config page will show initially or when the config shortcut is pressed
 })
 )V0G0N";
--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,31 @@
 
 stui::TabContainer* tabs;
 stui::ListView* artist_list;
+stui::TextArea* artist_detail_area;
+stui::ListView* artist_album_list;
 stui::ListView* album_list;
 stui::ListView* playlist_list;
 stui::ListView* track_list;
 
 void libraryTabChanged(int old_tab, int new_tab)
 {
-    switch (old_tab)
-    {
-        case 0: artist_list->enabled = false; break;
-        case 1: album_list->enabled = false; break;
-        case 2: playlist_list->enabled = false; break;
-        case 3: track_list->enabled = false; break;
-    }
-
-    switch (new_tab)
-    {
-        case 0: artist_list->enabled = true; break;
-        case 1: album_list->enabled = true; break;
-        case 2: playlist_list->enabled = true; break;
-        case 3: track_list->enabled = true; break;
-    }
 }
 
 int main()
 {
     stui::Terminal::configure("Simple Audio Library Tool", 1.0f);
+
     stui::LayoutReader reader;
     stui::Page* library_page = reader.readPage(library_page_str);
     artist_list = library_page->get<stui::ListView>("artist_list");
+    artist_detail_area = library_page->get<stui::TextArea>("artist_detail_area");
+    artist_album_list = library_page->get<stui::ListView>("artist_album_list");
     album_list = library_page->get<stui::ListView>("album_list");
     playlist_list = library_page->get<stui::ListView>("playlist_list");
     track_list = library_page->get<stui::ListView>("track_list");
     tabs = library_page->get<stui::TabContainer>("tabs");
-    library_page->focusable_component_sequence = { tabs, artist_list, album_list, playlist_list, track_list };
+    library_page->focusable_component_sequence = { tabs, artist_list, artist_detail_area, artist_album_list, album_list, playlist_list, track_list };
+    tabs->setEnabled(true); // force update the enabled children
     tabs->callback = libraryTabChanged;
     while (true)
     {
