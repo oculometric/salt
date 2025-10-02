295b21855bcc08bb661c201d64be0c9707ce69eb
jmac-lin-oculo <master.scratchman@gmail.com>
2025-10-02 16:46:04 +0100

    started building the ui

--- inc/ui.h
+++ inc/ui.h
@@ -3,7 +3,7 @@ R"V0G0N(
 HorizontalBox (children = {
     SizeLimiter (max_size = [24, -1], child = VerticalBox (children = {
         Label (text = "menu", alignment = 0),
-        ListView : "menu" (elements = { "artists", "albums", "tracks", "search", "import", "configure" })
+        ListView : "menu" (elements = { "artists", "albums", "tracks", "search", "import", "configure" }, show_numbers = 0)
         // Button : "art_but" (text = "artists"),
         // Button : "alb_but" (text = "albums"),
         // Button : "trk_but" (text = "tracks"),
@@ -18,12 +18,29 @@ HorizontalBox (children = {
 const char* library_page_str = 
 R"V0G0N(
 VerticalBox (children = {
-    TabDisplay : "tabs" (tab_descriptions = { "library", "search", "import", "config" }),
-    HorizontalBox (children = {
-        SizeLimiter (max_size = [24, -1], child = ListView : "menu" (elements = { "first", "second" })),
-        VerticalDivider (),
-        TextArea : "detail_area" (text = "details go here")
-    }),
+    TabContainer : "tabs" (children = 
+    {
+        HorizontalBox (children = {
+            SizeLimiter (max_size = [24, -1], child = ListView : "artist_list" (elements = { "first artist", "second" }, show_numbers = 0, enabled = 0)),
+            VerticalDivider (),
+            TextArea : "album_detail_area" (text = "artist details go here")
+        }),
+        HorizontalBox (children = {
+            SizeLimiter (max_size = [24, -1], child = ListView : "album_list" (elements = { "first album", "second" }, show_numbers = 0)),
+            VerticalDivider (),
+            TextArea : "album_detail_area" (text = "album details go here")
+        }),
+        HorizontalBox (children = {
+            SizeLimiter (max_size = [24, -1], child = ListView : "playlist_list" (elements = { "first playlist", "second", "third" }, show_numbers = 0, enabled = 0)),
+            VerticalDivider (),
+            TextArea : "playlist_detail_area" (text = "playlist details go here")
+        }),
+        HorizontalBox (children = {
+            SizeLimiter (max_size = [24, -1], child = ListView : "track_list" (elements = { "first track", "second" }, show_numbers = 0, enabled = 0)),
+            VerticalDivider (),
+            TextArea : "track_detail_area" (text = "track details go here")
+        })
+    }, tab_titles = { "artists", "albums", "playlists", "tracks" }, current_tab = 1),
     Label (text = "shortcut hints")
 })
 )V0G0N";
--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,48 @@
 #include "database.h"
 #include "ui.h"
 
+stui::TabContainer* tabs;
+stui::ListView* artist_list;
+stui::ListView* album_list;
+stui::ListView* playlist_list;
+stui::ListView* track_list;
+
+void libraryTabChanged(int old_tab, int new_tab)
+{
+    switch (old_tab)
+    {
+        case 0: artist_list->enabled = false; break;
+        case 1: album_list->enabled = false; break;
+        case 2: playlist_list->enabled = false; break;
+        case 3: track_list->enabled = false; break;
+    }
+
+    switch (new_tab)
+    {
+        case 0: artist_list->enabled = true; break;
+        case 1: album_list->enabled = true; break;
+        case 2: playlist_list->enabled = true; break;
+        case 3: track_list->enabled = true; break;
+    }
+}
+
 int main()
 {
     stui::Terminal::configure("Simple Audio Library Tool", 1.0f);
     stui::LayoutReader reader;
-    stui::Page* explore_page = reader.readPage(explore_page_str);
-    //explore_page->focusable_component_sequence = { (*explore_page)["art_but"], (*explore_page)["alb_but"], (*explore_page)["trk_but"], (*explore_page)["search_but"], (*explore_page)["import_but"], (*explore_page)["config_but"] };
-    explore_page->focusable_component_sequence = { (*explore_page)["menu"] };
+    stui::Page* library_page = reader.readPage(library_page_str);
+    artist_list = library_page->get<stui::ListView>("artist_list");
+    album_list = library_page->get<stui::ListView>("album_list");
+    playlist_list = library_page->get<stui::ListView>("playlist_list");
+    track_list = library_page->get<stui::ListView>("track_list");
+    tabs = library_page->get<stui::TabContainer>("tabs");
+    library_page->focusable_component_sequence = { tabs, artist_list, album_list, playlist_list, track_list };
+    tabs->callback = libraryTabChanged;
     while (true)
     {
-        explore_page->framerate(24);
-        explore_page->checkInput();
-        explore_page->render();
+        library_page->framerate(24);
+        library_page->checkInput();
+        library_page->render();
     }
     //Database d;
 }
